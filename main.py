e2e32762f434e9236c12648dac7e8e6f31116f55
Verball <chavezchristian50@gmail.com>
2025-05-22 09:25:17 -0700

    Modity main.py to accept argument rather than hard code location for files Modify stats.py to help manage helper functions for main.py

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
-def sort_on(dict):
-    return dict["count"]
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
-    res = {}
-    res_list = []
-    with open("./books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        for word in words:
-            for letter in word.lower():
-                if letter.isalpha():
-                    if letter not in res:
-                        res[letter] = 0
-                    res[letter] +=1
-        for key in res:
-            res_list.append({"character": key, "count": res[key]})
-        res_list.sort(reverse = True, key = sort_on)
-        print(res_list)
-        print("--- Begin report of books/franenstein.txt---")
-        print(f"{len(words)} words found in the document")
-        for small_dictionary in res_list:
-            #print(f"{small_dictionary['character']}")
-            print(f"The '{small_dictionary['character']}' character was found {small_dictionary['count']} times")
-        print("---End Report---")
-            
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
 main()
